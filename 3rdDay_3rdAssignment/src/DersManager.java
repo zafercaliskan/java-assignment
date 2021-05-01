1bbec2ee54ad6b22190172c1b3fcbf7f992b733c
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-05-01 16:55:19 +0300

    add manager

---
+++ 3rdDay_3rdAssignment/src/DersManager.java
@@ -0,0 +1,5 @@
+public class DersManager {
+	public void add(Ders ders) {
+		System.out.println(ders.getAdi() + " eklendi.");		
+	}
+}
---
+++ 3rdDay_3rdAssignment/src/EgitmenManager.java
@@ -0,0 +1,5 @@
+public class EgitmenManager {
+	public void add(Egitmen egitmen) {
+		System.out.println(egitmen.getFullName() + " egitmenId:" +egitmen.getEgitmenId()+ " eklendi.");
+	}
+}
