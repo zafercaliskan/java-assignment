1c37f5f82897eb6d6b90112b5ea4e2eff6330ed4
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-05-03 01:16:08 +0300

    Update Main.java

--- GameProject/src/Main.java
+++ GameProject/src/Main.java
@@ -1,15 +1,19 @@
 import Concrete.GamerCheckManager;
 import Concrete.GamerManager;
-import Concrete.MernisManager;
-import Concrete.SellManager;
+import Entities.Gamer;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
+		Gamer anil=new Gamer();
+		anil.setId(1);
+		anil.setFirstName("Anıl");
+		anil.setLastName("Ağhan");
+		anil.setTcNo("123456789112");
+		
 		GamerManager gamerManager = new GamerManager(new GamerCheckManager());
-		gamerManager.add(null);
-
+		gamerManager.add(anil);
 	}
 
 }
